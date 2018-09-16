57957ad9afef8a093bc965f92341a43d32ed1070
Vinicius Lira <vml@cin.ufpe.br>
2018-09-16 15:51:43 -0300

    exercicio rss completo

--- RSS/app/src/main/java/br/ufpe/cin/if710/rss/MainActivity.kt
+++ RSS/app/src/main/java/br/ufpe/cin/if710/rss/MainActivity.kt
@@ -2,11 +2,115 @@ package br.ufpe.cin.if710.rss
 
 import android.app.Activity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.view.View
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import java.io.ByteArrayOutputStream
+import java.io.IOException
+import java.io.InputStream
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlinx.android.synthetic.main.activity_main.*
+
+import java.nio.charset.Charset
 
 class MainActivity : Activity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private lateinit var RSS_FEED: String
+    private lateinit var linearLayoutManager: LinearLayoutManager
+    var feedRSS:List<ItemRSS> = emptyList()
+
+    protected override fun onCreate(savedInstanceState:Bundle?) {
         super.onCreate(savedInstanceState)
+        RSS_FEED = getString(R.string.rssfeed)
         setContentView(R.layout.activity_main)
+    }
+
+    protected override fun onStart() {
+        super.onStart()
+
+        //uso de doasync com anko
+        doAsync {
+
+            val feedXML = getRssFeed(RSS_FEED)
+            feedRSS = ParserRSS.parse(feedXML)
+
+            //atualiza a interface do user apos carregar os dados necessarios
+            uiThread {
+                try
+                {
+                    updateView()
+                }
+                catch (e:IOException) {
+                    e.printStackTrace()
+                }
+            }
+        }
+    }
+
+    //chama o conteudo carregado do xml getRSSFeed e organiza na interface
+    private fun updateView() {
+
+        // seta o adapter/os dados formatados da lista
+        val recyclerView = conteudoRSS
+        var adapter = RSSItemAdapter(feedRSS, this)
+        recyclerView.adapter = adapter
+
+        // seta o formato da lista
+        linearLayoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = linearLayoutManager
+
+        // define o listener para o clique no título do item do rss
+        adapter.setOnItemClickListener(object : RSSItemAdapter.OnItemClickListener {
+            override fun onClick(view: View, data: ItemRSS) {
+                loadWebpage(data.link)
+            }
+        })
+    }
+
+    //carrega o link numa webview
+    fun loadWebpage(url: String) {
+        webview.loadUrl("")
+        try {
+            webview.loadUrl(url)
+        } catch(e: UnsupportedOperationException) {
+            e.printStackTrace()
+        }
+    }
+
+    @Throws(IOException::class)
+    private fun getRssFeed(feed:String):String {
+        var input:InputStream? = null
+        var rssFeed = ""
+
+        //requisita conteudo do link
+        try
+        {
+            val url = URL(feed)
+            val conn = url.openConnection() as HttpURLConnection
+            input = conn.getInputStream()
+            val out = ByteArrayOutputStream()
+            val buffer = ByteArray(1024)
+            var count:Int = input.read(buffer)
+            while (count != -1) {
+                out.write(buffer, 0, count)
+                count = input.read(buffer)
+            }
+
+            val response = out.toByteArray()
+
+            val charset: Charset = Charsets.UTF_8
+
+            rssFeed = String(response, charset)
+        }
+
+        finally {
+            if (input != null)
+            {
+                input.close()
+            }
+        }
+        return rssFeed
     }
 }
---
+++ RSS/app/src/main/java/br/ufpe/cin/if710/rss/RSSItemAdapter.kt
@@ -0,0 +1,55 @@
+package br.ufpe.cin.if710.rss
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.RecyclerView.Adapter
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.itemlista.view.*
+
+
+//inicializa o adapter com os itens RSS
+class RSSItemAdapter(private val feedItems: List<ItemRSS>,
+                     private val context: Context) : Adapter<RSSItemAdapter.ViewHolder>() {
+
+    lateinit var listener: OnItemClickListener
+
+    override fun getItemCount(): Int {
+        return feedItems.size
+    }
+
+    //detecta que a acao de clique foi realizada no titulo da noticia
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val rssItem = feedItems[position]
+
+        holder.let {
+            it.title.text = rssItem.title
+            it.title.setOnClickListener({listener.onClick(it, rssItem)})
+            it.pubdate.text = rssItem.pubDate
+        }
+    }
+
+    //organiza os conteudos em itens separados na interface
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.itemlista, parent, false)
+        return ViewHolder(view)
+    }
+
+    //exibe as informacoes de titulo e data requisitadas
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)  {
+
+        val title = itemView.item_titulo
+        val pubdate = itemView.item_data
+
+    }
+
+    interface OnItemClickListener {
+        fun onClick(view: View, data: ItemRSS)
+    }
+
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        this.listener = listener
+    }
+
+}
